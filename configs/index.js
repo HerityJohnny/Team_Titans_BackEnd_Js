8dc61c6d4925fefe27964504f98eff01b5995702
Tobi Ajibade <herityjohnny14>
2020-06-11 15:08:01 -0700

    app working

--- configs/index.js
+++ configs/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
 module.exports = {
-	ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
-	AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
-	TWILIO_NUMBER: process.env.TWILIO_NUMBER,
+	ACCOUNT_SID: "AC4d02cc76ba2ba6a87eee0c7cf64bf567",//process.env.TWILIO_ACCOUNT_SID,
+	AUTH_TOKEN: "c1be285ec1e7d66736571b0584b3608f",//process.env.TWILIO_AUTH_TOKEN,
+	TWILIO_NUMBER: "+14439927056",//process.env.TWILIO_NUMBER,
 	MONGODBURI: process.env.MONGOURI || "mongodb://127.0.0.1:27017/sms-app",
 };
